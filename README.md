dfc210f384948e50c3c7496a8278ef8e419f5ed7
Nicholas Izekor <greatnicholas1@gmail.com>
2024-11-23 15:10:55 +0100

    second edition

---
+++ README.md
@@ -0,0 +1,9 @@
+daily message display
+npm init -y
+npm install express
+npm install nodemon -g // for the server to be restarting authomatically once u make changes to d code
+note: in d package.json , type:module to change to ESM for express to be activated, start:app.json
+creat folder (collector) with externsion of file name: mSg.json
+creat app.js as our landing page
+export mSg/js to app.js
+execute code. 
--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from 'express'; // local express application
-import daily from './collector/mSg.js'; // custom group 11
+import daily from './collector/mSg.js'; // custom app
 
 const server = express(); // express initiation 
 const PORT = 3000; // port assign
 
 //  middleware linking Home route
 
-server.use('/message', daily) // use in importing group11.js
+server.use('/message', daily) // use in importing mSg.js
 
 // 404 error middleware that end execution when all the above midleware couldnt meet the above condition
 server.use((req, res) => {
